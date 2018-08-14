4e9bfec3b6a537fa9ddaab89b9293b5193deffa5
DavidThompsonZA <david@parcelninja.com>
2018-08-14 11:12:02 +0200

    more

--- pwabuilder-sw-register.js
+++ pwabuilder-sw-register.js
@@ -3,16 +3,16 @@
 //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 try{
   if (navigator.serviceWorker.controller) {
-    console.log('[PWA Builder] active service worker found, no need to register')
+    alertify.message('[PWA Builder] active service worker found, no need to register')
   } else {
     //Register the ServiceWorker
     navigator.serviceWorker.register('pwabuilder-sw.js', {
       scope: './'
     }).then(function(reg) {
-      console.log('Service worker has been registered for scope:'+ reg.scope);
-    }).catch((e) => handleError(e));
+      alertify.success('Service worker has been registered for scope:'+ reg.scope);
+    }).catch((e) => handleError(e, "Register Service Worker 'pwabuilder-sw.js' Error"));
   }
 }
 catch (e) {
-  handleError(e);
+  handleError(e, "Register Service Worker 'pwabuilder-sw.js' TryCatch Error");
 }
